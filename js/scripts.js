a7ec98c90173855f647361c71c62936726b1d871
Saoud <saoud@saoud.dev>
2021-02-03 20:26:25 -0800

    adding logic to scripts.js
    Co-authored-by: Cat Denton <willwdenton@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,42 @@
+
+function chooseResult(sum) {
+  if (sum < 0) {
+    $('#great').show();
+    $('#notGreat').hide();
+    $('#seekHelp').hide();
+  } else if (sum >= 0 && sum <6) {
+    $('#notGreat').show();
+    $('#great').hide();
+    $('#seekHelp').hide();
+  } else {
+    $('#seekHelp').show();
+    $('#great').hide();
+    $('#notGreat').hide();
+  }
+}  
+
 $(document).ready(function() {
-  $('form#stress-test').submit(function(event) {
-    event.preventDefault();
+  $("form#stress-test").submit(function(event) {
     
+    let total=0;
+    
+    $("input:checkbox[name=effects]:checked").each(function() {
+      const number = $(this).val()
+      total += parseInt(number)
+    });
+
+    $("input:checkbox[name=symptoms]:checked").each(function() {
+      const number = $(this).val()
+      total += parseInt(number)
+    });
+
+    $("input:checkbox[name=coping-methods]:checked").each(function() {
+      const number = $(this).val()
+      total += parseInt(number)
+    });
+
+    chooseResult(total);
+
+    event.preventDefault();
   }); 
 });
